53fd0676ddb99333d1691f29abfc71956617ea26
Windanim <chris@fred.kiwi>
2022-02-22 07:45:07 -0800

    Began building recipe class

--- cookbook.py
+++ cookbook.py
@@ -1,9 +1,33 @@
 import json
 import os
 import sys
-import codeToolkit
+import _toolkit as ctk # My personal toolkit - This contains a few helper functions that I reuse regularly
 
-codeToolkit.clearConsole()
-codeToolkit.pageBreak()
-codeToolkit.declareScriptEnd()
-codeToolkit.pageBreak()
+
+class recipe:
+
+    def __init__(self, name) -> None:
+        self.name = name
+        self.ingredients = []
+        self.cookingTools = []
+        self.steps = []
+        self.cookingTime = 0
+        self.price = 0
+
+    def addIngredient(self):
+        pass
+
+    def calculatePriceOfIngredients(self):
+        self.price = sum([ingredient['price'] for ingredient in self.ingredients])
+
+    def addCookingTool(self):
+        pass
+
+    def addStep(self):
+        pass
+
+if __name__ == '__main__':
+    ctk.clearConsole()
+    ctk.pageBreak()
+    ctk.declareScriptEnd()
+    ctk.pageBreak()
