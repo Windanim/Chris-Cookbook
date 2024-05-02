cd99616a0e0cd54ed749d162738903076bf8fadc
Chris Frederickson <100047424+fred-kiwi@users.noreply.github.com>
2024-05-02 16:29:27 -0700

    Committing old updates

--- cookbook.py
+++ cookbook.py
@@ -3,103 +3,205 @@ import re
 import json
 import os
 import sys
-import _toolkit as ctk # My personal toolkit - This contains a few helper functions that I reuse regularly
+from time import sleep
+import pprint
+# - I SHOULD SEE IF I CAN FIND A KITCHEN MEASUREMENTS CONVERSION TABLE
 
 # --- CLASSES BLOCK
 
-# -- Cupboard Classes
+#class KitchenCupboard: # - I NEED TO ADD INGREDIENTS AND TOOLS HERE FOR EASY ACCESS LATER # - CURRENTLY IRRELEVANT, TO BE WORKED IN LATER
+#
+#    def __init__(self) -> None:
+#        pass
 
-class cupboard:
+prettyPrintDict = pprint.PrettyPrinter(indent=4)
 
-    def __init__(self) -> None:
-        self.cupboardItems = {}
+class RecipeBook:
 
-    def __repr__(self) -> str:
-        return 'Available Cupboard Items:\n' + str(list(self.cupboardItems.keys()))
+    class __RecipeEncoder(json.JSONEncoder):
+        def default(self, recipeObject):
+            return recipeObject.__dict__
 
-class cupboardItem:
-
-    def __init__(self, name, measuredIn, amountInCupboard) -> None:
-        self.name = name
-        self.measuredIn = measuredIn
-        self.amountInCupboard = amountInCupboard
-
-# -- Recipe Classes
-
-class recipeBook:
-
-    def __init__(self) -> None:
+    def __init__(self):
         self.recipes = {}
 
-    def __repr__(self) -> str:
-        return 'Available Recipes:\n' + str(list(self.recipes.keys()))
+    def addRecipe(self):
 
-class recipe:
+        nameOfNewRecipe = input('What is the name of this recipe?:\n')
+        newRecipe = Recipe(nameOfNewRecipe)
+        newRecipe.addCookingTools()
+        newRecipe.addIngredients()
+        newRecipe.addSteps()
 
-    class ingredient:
+        self.recipes[newRecipe.name] = self.__RecipeEncoder().encode(newRecipe)
 
-        def __init__(self, nameOfIngredient, amountNeeded) -> None:
-            self.nameOfIngredient = nameOfIngredient
-            self.amountNeeded = amountNeeded
+        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), F'/recipes/{newRecipe.name}.json'), 'a') as recipeFile:
+            recipeFile.seek(0)
+            json.dump(self.__RecipeEncoder().encode(newRecipe), recipeFile)
+            recipeFile.truncate()
+        
+        # - I NEED TO STORE THE RECIPE AS A JSON FILE TO BE ACCESSED LATER
 
-            # When using recipe for shopping order:
-            # CHECK TO SEE IF INGREDIENT IS IN THE CUPBOARD HERE (WITH FUZZY MATCHING)
-            # REMOVE THE AMOUNT NEEDED FROM THE CUPBOARD IF PRESENT
-            # ADD IT TO THE SHOPPING LIST IF NOT, OR IF LESS EXISTS THAN IS REQUIRED
+class Ingredient:
+
+    def __init__(self, nameOfIngredient, amountNeeded, measuredIn) -> None:
+        self.nameOfIngredient = nameOfIngredient
+        self.amountNeeded = amountNeeded
+        self.measuredIn = measuredIn
+
+class Recipe:
 
     def __init__(self, name) -> None:
         self.name = name
-        self.ingredients = []
         self.cookingTools = []
-        self.steps = []
-        self.cookingTime = 0
-        self.price = 0
+        self.ingredients = []
+        self.steps = {}
+        self.numOfSteps = 0
+        #self.cookingTime = 0 # - CURRENTLY IRRELEVANT, TO BE WORKED IN LATER
+        #self.tags = [] # - CURRENTLY IRRELEVANT, TO BE WORKED IN LATER
+        #self.price = 0 # - CURRENTLY IRRELEVANT, TO BE WORKED IN LATER
+
+    def __questionLoop(self, nameOfQuestionLoop):
+
+        sleep(0.5)
+
+        pageBreak()
+
+        questionLoopResponse = str(input(F'\n\nWould you like to add any more {nameOfQuestionLoop}s to this recipe? (y/n) ')).lower().strip()
+        if questionLoopResponse == 'y':
+            return questionLoopResponse
+        elif questionLoopResponse != 'n':
+            print('I\'m, sorry, I didn\'t understand your input...')
+            self.__questionLoop(nameOfQuestionLoop)
+
+    def addCookingTools(self):
+
+        sleep(0.5)
+
+        clearConsole()
+        pageBreak()
+
+        print('Currently editing recipe: ' + self.name + '\n\nCurrently added cooking tools:\n' + str(self.cookingTools))
+
+        pageBreak()
+
+        nameOfCookingToolToAdd = input('Name of the cooking tool to add:\n')
+        self.cookingTools.append(nameOfCookingToolToAdd)
+
+        clearConsole()
+        pageBreak()
+
+        print('Currently editing recipe: ' + self.name + '\n\nCurrently added cooking tools:\n' + str(self.cookingTools))
+
+        moreCookingToolsResponse = self.__questionLoop('cooking tool')
+        if moreCookingToolsResponse == 'y':
+            self.addCookingTools()
 
     def addIngredients(self):
 
-        def addIngredientsQuestionLoop(self):
+        sleep(0.5)
 
-            ctk.pageBreak()
+        clearConsole()
+        pageBreak()
 
-            moreIngredientsResponse = str(input('\n\nWould you like to add any more ingredients to this recipe? (y/n)')).lower().strip()
-            if moreIngredientsResponse == 'y':
-                return moreIngredientsResponse
-            elif moreIngredientsResponse != 'n':
-                print('I\'m, sorry, I didn\'t understand your input...')
-                addIngredientsQuestionLoop()
+        print('Currently editing recipe: ' + self.name + '\n\nCurrently added ingredients:\n' + str([ingredientListed.nameOfIngredient for ingredientListed in self.ingredients]))
 
-        ctk.clearConsole()
-        ctk.pagebreak()
+        pageBreak()
 
-        nameOfIngredientToAdd = input('Name of the ingredient to add: ') # WILL NEED TO VERIFY THAT THIS EXISTS IN THE CUPBOARD & ADD IT IF NOT
-        amountOfIngredientNeeded = input('Amount needed: ') # WILL NEED TO VERIFY THAT THIS IS A NUMBER
-        ingredientToAdd = self.ingredient(str(nameOfIngredientToAdd), int(amountOfIngredientNeeded))
+        nameOfIngredientToAdd = input('Name of the ingredient to add:\n')
+        ingredientMeasuredIn = input('\nWhat will we use to measure this ingredient? (Tbsp, tsp, kg, g, cups, etc.):\n')
+        isIngredientAmountANumber = False
+        while isIngredientAmountANumber == False:
+            try:
+                amountOfIngredientNeeded = input('\nAmount needed (Numbers only):\n') # - I NEED TO VERIFY
+                verifiedAmountOfIngredientNeeded = float(amountOfIngredientNeeded)
+                isIngredientAmountANumber = True
+            except:
+                print('\nError - please input a number.')
+                sleep(3)
+                clearConsole()
+                pageBreak()
+                print('Currently editing recipe: ' + self.name + '\n\nCurrently added ingredients:\n' + str([ingredientListed.nameOfIngredient for ingredientListed in self.ingredients]))
+                pageBreak()
+                print(F'Name of the ingredient to add:\n{nameOfIngredientToAdd}')
+                print(F'\nWhat will we use to measure this ingredient? (Tbsp, tsp, kg, g, cups, etc.):\n{ingredientMeasuredIn}')
+
+        ingredientToAdd = Ingredient(str(nameOfIngredientToAdd), float(verifiedAmountOfIngredientNeeded), str(ingredientMeasuredIn))
         self.ingredients.append(ingredientToAdd)
 
-        ctk.clearConsole()
-        ctk.pageBreak()
+        clearConsole()
+        pageBreak()
 
-        print('Currently editing recipe: ' + self.name + 'Currently added ingredients:\n' + str([ingredientListed.nameOfIngredient for ingredientListed in self.ingredients]))
+        print('Currently editing recipe: ' + self.name + '\n\nCurrently added ingredients:\n' + str([ingredientListed.nameOfIngredient for ingredientListed in self.ingredients]))
 
-        moreIngredientsResponse = addIngredientsQuestionLoop()
+        moreIngredientsResponse = self.__questionLoop('ingredient')
         if moreIngredientsResponse == 'y':
             self.addIngredients()
 
-    # Will have to flesh this bit out later & likely change where it lives in the code
-    # def calculatePriceOfIngredients(self):
-    #     self.price = sum([ingredient['price'] for ingredient in self.ingredients])
+    def addSteps(self):
 
-    def addCookingTools(self):
-        pass
+        sleep(0.5)
 
-    def addStep(self):
-        pass
+        clearConsole()
+        pageBreak()
+
+        print('Currently editing recipe: ' + self.name + '\n\nCurrently added steps:\n' + str(self.numOfSteps))
+
+        pageBreak()
+
+        descriptionOfStepToAdd = input('Please describe the current step:\n')
+        self.steps[str(self.numOfSteps + 1)] = descriptionOfStepToAdd
+        self.numOfSteps = len(list(self.steps.keys()))
+
+        clearConsole()
+        pageBreak()
+
+        print('Currently editing recipe: ' + self.name + '\n\nCurrently added steps:\n' + str(self.numOfSteps))
+
+        moreStepsResponse = self.__questionLoop('step')
+        if moreStepsResponse == 'y':
+            self.addSteps()
+
+
+# ---
+
+# --- FUNCTION BLOCK
+
+def pageBreak():
+    print('\n---\n')
+
+def clearConsole():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+# ---
+
+def main():
+
+    # - I NEED TO CHECK FOR PREVIOUSLY STASHED RECIPE FILES
+    # - I NEED TO SPIN UP A NEW COOKBOOK USING ANY EXISTING FILES AS A BASE
+    # - I NEED TO ASK USER INPUT FOR WHAT THEY WOULD LIKE TO DO
+
+    #print(os.path.join(os.path.dirname(os.path.realpath(__file__)), '/recipes/'))
+    if not os.path.exists(os.path.join(os.path.dirname(os.path.realpath(__file__)), '/recipes/')): # - I NEED TO FIX THIS PATH LATER
+        os.mkdir((os.path.join(os.path.dirname(os.path.realpath(__file__)), '/recipes/')))
+
+    clearConsole()
+    pageBreak()
+
+    recipeBookForCurrentRun = RecipeBook()
+    recipeBookForCurrentRun.addRecipe()
+
+    encodedRecipe = recipeBookForCurrentRun.recipes[list(recipeBookForCurrentRun.recipes.keys())[0]]
+
+    clearConsole()
+    pageBreak()
+
+    prettyPrintDict.pprint(encodedRecipe)
+
+    pageBreak()
 
 # --- RUN BLOCK
 
 if __name__ == '__main__':
-
-    ctk.clearConsole()
-    ctk.pageBreak()
-    ctk.declareScriptEnd()
-    ctk.pageBreak()
+    
+    main()
